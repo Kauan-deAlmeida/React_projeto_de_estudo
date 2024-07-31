8e6481d883ab9ed2086c0c63d51270bac28a95a3
Kauan-deAlmeida <kauan.almeida@sptech.school>
2024-07-31 20:43:51 -0300

    Inicio do projeto de estudo para REACT, fazendo alteração basico na página app.js

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const name = 'Kauan'
+  const newName = name.toUpperCase()
+
+  function sum(a, b){
+    return a + b
+  }
+
+  const url = "https://via.placeholder.com/150"
+
+  return (
+    <div className="App">
+      <h1>Ola React!</h1>
+      <p>Meu primeiro projeto</p>
+      <p>Olá, {newName}</p>
+      <p>Soma: {sum(1, 2)}</p>
+      <img src={url} alt="Minha imagem" />
+    </div>
+  );
+}
+
+export default App;
